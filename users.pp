2ca76b87c800ca3bca2cee677abaa51cb13723b6
root <root@ip-172-31-4-101.us-west-2.compute.internal>
2017-10-12 13:22:43 +0000

    added other users

--- users.pp
+++ users.pp
@@ -6,3 +6,22 @@ user { 'becca' :
     groups     => ['sysadmin', 'cars'],
     shell      => '/bin/bash',
   }
+
+user { 'fred' :
+    ensure     => present,
+    uid        => '10025566',
+    home       => '/home/fred',
+    managehome => true,
+    groups     => ['trucks', 'cars', 'wheel'],
+    shell      => '/bin/csh',
+  }
+
+  user { 'wilma' :
+    ensure         => present,
+    uid            => '10035566',
+    home           => '/home/wilma',
+    managehome     => true,
+    groups         => ['trucks', 'cars', 'ambulances'],
+    shell          => '/bin/bash',
+    purge_ssh_keys => true,
+  }
