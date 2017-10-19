1a96d272a5e40e60e6906d178afa28ca3c76b086
root <root@ip-172-31-4-249.us-west-2.compute.internal>
2017-10-19 02:32:45 +0000

    dia2code installed

--- s3485566-usap-a2/packages.pp
+++ s3485566-usap-a2/packages.pp
@@ -10,3 +10,11 @@ package { $packages :
 	ensure   => 'installed',
 	provider => 'yum',
 }
+package { 'libxml2.so.2' :
+    ensure => installed,
+  }
+package { 'dia2code' :
+	ensure   => 'installed',
+	source   => 'https://downloads.sourceforge.net/project/dia2code/dia2code/0.8.3/dia2code-0.8.3-3.1.i586.rpm?r=http%3A%2F%2Fdia2code.sourceforge.net%2Fdownload.html&ts=1507292790&use_mirror=ncu',
+	provider => 'rpm',
+  }
