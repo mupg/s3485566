e1c493a9e44edc8e371a03304282ed245b712d7a
root <root@ip-172-31-4-249.us-west-2.compute.internal>
2017-10-19 01:04:06 +0000

    added passwords and wilmas ssh key

--- s3485566-usap-a2/users.pp
+++ s3485566-usap-a2/users.pp
@@ -1,3 +1,5 @@
+class user{
+require groups
 user { 'becca' :
     ensure     => present,
     uid        => '10015566',
@@ -5,6 +7,7 @@ user { 'becca' :
     managehome => true,
     groups     => ['sysadmin', 'cars'],
     shell      => '/bin/bash',
+    password   => '$1$6rSaqCiq$SPCeuTEtOV34oEKlC9UPc' #plain text is my student number
   }
 
 user { 'fred' :
@@ -25,3 +28,10 @@ user { 'fred' :
     shell          => '/bin/bash',
     purge_ssh_keys => true,
   }
+ssh_authorized_key { 'wilmaKey' :
+	ensure => present,
+	user   => 'wilma',
+	type   => 'ssh-rsa',
+	key    => 'PuBlIcKeYWilMaAa',
+}
+}
