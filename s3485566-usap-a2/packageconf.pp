1ca5fbe69f648945e093335c5a9c77775496463c
root <root@ip-172-31-4-249.us-west-2.compute.internal>
2017-10-20 12:37:34 +0000

    added some agent and package management, interval still not applying

--- s3485566-usap-a2/packageconf.pp
+++ s3485566-usap-a2/packageconf.pp
@@ -1,5 +1,23 @@
 class packageconf{
-{ 'runlevel' :
+
+exec { 'runlevel' :
 	command => '/usr/bin/systemctl set-default multi-user.target',
 }
+
+file { '/etc/ssh/sshd_config':
+	ensure => present,
+	mode => '0777',
+	owner => 'root',
+	group => 'root',
+	source => '/etc/puppetlabs/code/environments/production/manifests/s3485566-usap-a2/sshd_config',
+	require => Package['openssh-server'],
+}
+service { 'sshd' :
+	ensure => running,
+	enable => true,
+	require => Package['openssh-server'],
+	subscribe => File['/etc/ssh/sshd_config'],
+}
+
+
 }
