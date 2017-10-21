52e9fbf6c7b28e3c7736ac59661f425bda3bd5ca
root <root@ip-172-31-4-249.us-west-2.compute.internal>
2017-10-21 02:57:20 +0000

    added things to packageconf

--- s3485566-usap-a2/packageconf.pp
+++ s3485566-usap-a2/packageconf.pp
@@ -18,6 +18,37 @@ service { 'sshd' :
 	require => Package['openssh-server'],
 	subscribe => File['/etc/ssh/sshd_config'],
 }
+file {'/var/www/s3485566':
+	ensure => 'directory',
+	owner  => 'root',
+	mode   => '0644',
+}
+exec { 'becca_sudo' :
+command => '/usr/sbin/usermod -aG wheel becca',
+}
+file {'/etc/profile.d/setuserbin.sh' :
+	owner => 'root',
+	mode => '0644',
+	content => 'PATH=$PATH:/usr/local/bin',
+}
+exec { 'mount_becca_titan' :
+	command => '/usr/bin/mkdir /home/becca/titan; 
+	echo \ "$(cat /etc/puppetlabs/code/environments/production/manifests/ssh_passwd)"| \
+	/usr/bin/sshfs -o StrictHostKeyChecking=no -o password_stdin
+	s3485566@titan.csit.rmit.edu.au:/home/sh6/s3485566/ /home/becca/titan/',
+	unless => '/usr/bin/find /home/becca/titan mindepth 1 | /usr/bin/read',
+}
 
-
+host{'titan.csit.rmit.edu.au':
+        ip => '131.170.5.132',
+        host_aliases => 'titan',
+}
+host{'saturn.csit.rmit.edu.au':
+	ip => '131.170.5.132',
+	host_aliases => 'saturn',
+}
+host{'jupiter.csit.rmit.edu.au':
+	ip => '131.170.5.135',
+	host_aliases => 'jupiter',
+}
 }
